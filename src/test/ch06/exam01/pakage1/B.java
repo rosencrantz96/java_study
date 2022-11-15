1e550af0917e197e2f15444db2ba112250c578d6
rosencrantz96 <topgdvidsyb@naver.com>
2022-11-15 18:34:18 +0900

    up

--- src/test/ch06/exam01/pakage1/B.java
+++ src/test/ch06/exam01/pakage1/B.java
@@ -4,5 +4,5 @@ public class B {
 //	A a = new A(); // A클래스(default) 접근 가능 
 	A a1 = new A(true); // ok 
 	A a2 = new A(1); // ok
-	A a3 = new A("문자열"); // private라서 안됨 
+//	A a3 = new A("문자열"); // private라서 안됨 
 }
--- src/test/ch06/exam01/pakage2/C.java
+++ src/test/ch06/exam01/pakage2/C.java
@@ -8,7 +8,7 @@ public class C {
 //	B b = new B(); // B클래스(public)은 어디서든지 접근 가능 (import 해주면 됨)
 	
 	A a1 = new A(true); // ok 
-	A a2 = new A(1); // X 패키지가 다르기 때문에 에러가 난다 (A클래스는 default니까)
-	A a3 = new A("문자열"); // X	(얘는 private 라서 에러남)
+//	A a2 = new A(1); // X 패키지가 다르기 때문에 에러가 난다 (A클래스는 default니까)
+//	A a3 = new A("문자열"); // X	(얘는 private 라서 에러남)
 
 }
