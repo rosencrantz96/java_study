62244b380c3cfa04b3e7b9e6e16c783a0fdf8553
rosencrantz96 <topgdvidsyb@naver.com>
2022-11-30 13:37:46 +0900

    화재?

--- src/test/ch12/method/GenericEx.java
+++ src/test/ch12/method/GenericEx.java
@@ -12,9 +12,10 @@ public class GenericEx {
 		//제네릭 메소드 호출: T를 Integer로 대체 (모든 T의 자리를 대체한다고 생각하세요)
 		Box<Integer> box1 = boxing(100); // Integer라는 타입파라메터를 줘서 t는 100이되고 Box 객체생성 
 		int intValue = box1.get(); // 강제형변환 필요 없이 바로 가져올 수 있당 제네릭 짱
+		System.out.println(box1.get()); 
 		System.out.println(intValue);
 		
-		Box<String> box2 = boxing("존나춥네"); // boxing 메소드 실현하는 순간 객체가 생성되고 메소드 set메소드 호출
+		Box<String> box2 = boxing("추운날"); // boxing 메소드 실현하는 순간 객체가 생성되고 메소드 set메소드 호출
 		String strValue = box2.get();
 		System.out.println(strValue);
 		
--- src/test/ch12/wild/Applicant.java
+++ src/test/ch12/wild/Applicant.java
@@ -2,6 +2,7 @@ package test.ch12.wild;
 
 public class Applicant<T> {
 	public T kind;
+//	public Person kind = new Person();
 	
 	// 생성자 
 	public Applicant(T kind) {
